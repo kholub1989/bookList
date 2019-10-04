cbf90ab5b75a8352fd048255c859975a624acb32
Krisztian Holub <kholub1989@gmail.com>
2019-10-04 14:47:31 -0400

    Delete Book form List

--- app.js
+++ app.js
@@ -44,6 +44,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -51,7 +58,7 @@ UI.prototype.clearFields = function() {
   document.getElementById('isbn').value = '';
 }
 
-// Event Listeners
+// Event Listeners for Add Book
 document.getElementById('book-form').addEventListener('submit', function(e){
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
@@ -81,3 +88,17 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 });
+
+// Event Listener for Delete
+document.getElementById('book-list').addEventListener('click', function(e) {
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
+
+  e.preventDefault();
+})
